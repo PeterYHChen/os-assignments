1aba37087d47c710b5634353fdae2950bc4e8167
yonghong <yonghong.chenn@gmail.com>
2017-04-21 11:38:50 -0400

    finish lru, not fix bugs yet

--- lab4-paging/lab4-paging.cpp
+++ lab4-paging/lab4-paging.cpp
@@ -24,18 +24,21 @@ using namespace std;
 struct Process
 {
     int id;
-    vector<bool> pageAllocated;
+    vector<int> pageToFrameIndex;
     int size;
     int refCnt;
     double A, B, C;
     int refIndex;
+    int pageFault;
+    double totalResidencyTime;
+    int numOfEvictions;
 };
 
 struct Page
 {
     int allocatedTime;
     int referencedTime;
-    int startRefIndex;
+    int refPageIndex;
     int processId; // processId = -1 indicates avaliable frame
 };
 
@@ -62,12 +65,13 @@ void printVector(vector<Process> &procList)
     for (vector<Process>::iterator i = procList.begin(); i != procList.end(); ++i)
     {
         printf("Process id: %d\n", i->id);
-        printf("    page table size: %d\n", i->pageAllocated.size());
+        printf("    page table size: %ld\n", i->pageToFrameIndex.size());
         printf("    size: %d\n", i->size);
         printf("    refCnt: %d\n", i->refCnt);
         printf("    refIndex: %d\n", i->refIndex);
         printf("    A: %1.3f B: %1.3f C: %1.3f\n", i->A, i->B, i->C);
     }
+    cout << endl;
 }
 
 void initiate(int machineSize, int pageSize, int procSize, int jobMix, int refCnt)
@@ -80,69 +84,100 @@ void initiate(int machineSize, int pageSize, int procSize, int jobMix, int refCn
     for (vector<Page>::iterator iter = frameTable.begin(); iter != frameTable.end(); iter++)
         iter->processId = -1; // indicate empty frame
 
-    // initiate vector of process list
-    Process temp;
-    temp.pageAllocated.resize(procSize/pageSize + 1, false);
-    temp.size = procSize;
-    temp.refCnt = refCnt;
-    temp.refIndex = -1; // indicates no referenceIndex at the beginning
+    // initiate vector of process list, init page to frame index to be -1 indicates not allocated
+    Process proc;
+    proc.pageToFrameIndex.resize(procSize/pageSize + 1, -1);
+    proc.size = procSize;
+    proc.refCnt = refCnt;
+    proc.refIndex = -1; // indicates no referenceIndex at the beginning
+    proc.pageFault = 0;
+    proc.totalResidencyTime = 0;
+    proc.numOfEvictions = 0;
 
     switch (jobMix)
     {
         case 1:
-            temp.id = 1;
-            temp.A = 1;
-            temp.B = 0;
-            temp.C = 0;
-            procList.push_back(temp);
+            proc.id = 1;
+            proc.A = 1;
+            proc.B = 0;
+            proc.C = 0;
+            procList.push_back(proc);
             break;
         case 2:
-            temp.A = 1;
-            temp.B = 0;
-            temp.C = 0;
+            proc.A = 1;
+            proc.B = 0;
+            proc.C = 0;
             for (int i = 0; i < 4; i++)
             {
-                temp.id = i+1;
-                procList.push_back(temp);
+                proc.id = i+1;
+                procList.push_back(proc);
             }
             break;
         case 3:
-            temp.A = 0;
-            temp.B = 0;
-            temp.C = 0;
+            proc.A = 0;
+            proc.B = 0;
+            proc.C = 0;
             for (int i = 0; i < 4; i++)
             {
-                temp.id = i+1;
-                procList.push_back(temp);
+                proc.id = i+1;
+                procList.push_back(proc);
             }
             break;
         case 4:
-            temp.id = 1;
-            temp.A = 0.75;
-            temp.B = 0.25;
-            temp.C = 0;
-            procList.push_back(temp);
-            temp.id = 2;
-            temp.A = 0.75;
-            temp.B = 0;
-            temp.C = 0.25;
-            procList.push_back(temp);
-            temp.id = 3;
-            temp.A = 0.75;
-            temp.B = 0.125;
-            temp.C = 0.125;
-            procList.push_back(temp);
-            temp.id = 4;
-            temp.A = 0.5;
-            temp.B = 0.125;
-            temp.C = 0.125;
-            procList.push_back(temp);
+            proc.id = 1;
+            proc.A = 0.75;
+            proc.B = 0.25;
+            proc.C = 0;
+            procList.push_back(proc);
+            proc.id = 2;
+            proc.A = 0.75;
+            proc.B = 0;
+            proc.C = 0.25;
+            procList.push_back(proc);
+            proc.id = 3;
+            proc.A = 0.75;
+            proc.B = 0.125;
+            proc.C = 0.125;
+            procList.push_back(proc);
+            proc.id = 4;
+            proc.A = 0.5;
+            proc.B = 0.125;
+            proc.C = 0.125;
+            procList.push_back(proc);
             break;
         default:
             cout << "Error. Unrecognized jobMix value: " << jobMix << endl;
     }
 }
 
+void printSummary()
+{
+    cout << endl;
+
+    int totalPageFault = 0;
+    double totalResidencyTime = 0;
+    int totalNumOfEvictions = 0;
+    for (vector<Process>::iterator currProc = procList.begin(); currProc != procList.end(); currProc++)
+    {
+        totalPageFault += currProc->pageFault;
+        totalResidencyTime += currProc->totalResidencyTime;
+        totalNumOfEvictions += currProc->numOfEvictions;
+        if (currProc->numOfEvictions > 0)
+            printf("Process %d had %d faults and %f average residency.\n", 
+                currProc->id, currProc->pageFault, currProc->totalResidencyTime / currProc->numOfEvictions);
+        else
+            printf("Process %d had %d faults.\n\tWith no evictions, the average residence is undefined.\n", 
+                currProc->id, currProc->pageFault);
+
+    }
+
+    if (totalNumOfEvictions > 0)
+        printf("\nThe total number of faults is %d and the overall average residency is %f.\n\n", 
+            totalPageFault, totalResidencyTime / totalNumOfEvictions);
+    else
+        printf("\nThe total number of faults is %d.\n\tWith no evictions, the overall average residence is undefined.\n\n", 
+            totalPageFault);
+}
 void LIFO()
 {
 
@@ -153,105 +188,121 @@ void RANDOM()
     
 }
 
-class compare
-{
-public:
-    bool operator() (const Page* a, const Page* b) const
-    {
-        return a->referencedTime > b->referencedTime;
-    }
-};
-
 void LRU()
 {
     int time = 0;
     int quantum = 3;
 
-    priority_queue<Page*, vector<Page*>, compare> lruQ;
-
     while (true)
     {
         int noRefProcessCnt = 0;
+        int nextRefIndex;
         for (vector<Process>::iterator currProc = procList.begin(); currProc != procList.end(); currProc++)
         {
-            // no more reference of current process
-            if (currProc->refCnt <= 0)
-            {
-                noRefProcessCnt++;
-                continue;
-            }
-
             // begin round robin of quantum 3
             for (int i = 0; i < quantum; i++)
             {
+                // no more reference of current process
+                if (currProc->refCnt <= 0)
+                {
+                    noRefProcessCnt++;
+                    break;
+                }
+                currProc->refCnt--;
+
                 time++;
-                // the first reference, 111*k mod S
+
+                // if this is the first reference, 111*k mod S
                 if (currProc->refIndex < 0)
                     currProc->refIndex = (currProc->id * 111) % currProc->size;
                 else
-                {
-                    double y = getRandomNumber() / (RAND_MAX + 1.0);
-                    if (y < currProc->A)
-                        currProc->refIndex += 1;
-                    else if (y < currProc->A + currProc->B)
-                        currProc->refIndex -= 5;
-                    else if (y < currProc->A + currProc->B + currProc->C)
-                        currProc->refIndex += 4;
-                    else
-                        currProc->refIndex = getRandomNumber();
+                    currProc->refIndex = nextRefIndex;
 
-                    currProc->refIndex %= currProc->size;
-                }
-
-                int refPageIndex = currProc->refIndex/pageSize;
-                DEBUG(printf("references word %d (page %d) at time %d", currProc->refIndex, refPageIndex, time);)
+                int currRefPageIndex = currProc->refIndex/pageSize;
+                DEBUG(printf("Process %d references word %d (page %d) at time %d", currProc->id, currProc->refIndex, currRefPageIndex, time);)
 
                 // if this reference index has been allocated to a frame
-                if (currProc->pageAllocated[refPageIndex])
+                int currFrameIndex = currProc->pageToFrameIndex[currRefPageIndex];
+                if (currFrameIndex >= 0)
                 {
-                    DEBUG(printf(" - Hit in frame\n");)
+                    // update referenced time of the page in frame
+                    frameTable[currFrameIndex].referencedTime = time;
+                    DEBUG(printf(" - Hit in frame %d\n", currFrameIndex);)
                 }
-                // not allocate to a frame before
+                // not allocate to a frame before, page fault
                 else
                 {
-                    bool finishAllocation = false;
+                    currProc->pageFault++;
 
+                    int targetFrameIndex;
+                    vector<Page>::iterator targetPage;
                     // search frame table from the end to the beginning for free frame
-                    for (vector<Page>::iterator currPage = frameTable.end() - 1; currPage >= frameTable.begin(); currPage--)
+                    for (targetPage = frameTable.end() - 1; targetPage >= frameTable.begin(); targetPage--)
                     {
-                        if (finishAllocation)
-                            break;
+                        targetFrameIndex = targetPage - frameTable.begin();
 
-                        // find a free frame for a page
-                        if (currPage->processId < 0)
+                        // id < 0 means found a free frame for a page
+                        if (targetPage->processId < 0)
                         {
-                            currPage->allocatedTime = time;
-                            currPage->referencedTime = time;
-                            currPage->startRefIndex = currProc->refIndex;
-                            currPage->processId = currProc->id;
-
-                            // loop over words in the same page to indicate being allocated
-                            int currRefIndex = currProc->refIndex + j;
-                            for (int j = 0; j < pageSize; j++)
-                            {
-                                // ensure reference index is inside the process 
-                                int currRefIndex = currProc->refIndex + j;
-                                if (currRefIndex >= currProc->size)
-                                    break;
-                                currProc->pageAllocated[currProc->refIndex + j] = true;
-                            }
-
-                            finishAllocation = true;
+                            DEBUG(printf(" - Fault, using free frame %d\n", targetFrameIndex);)
+                            break;
                         }
                     }
 
-                    // frame table is full, evict a page, record residence time
-                    if (!finishAllocation)
+                    // if frame table is full, evict a page, record residence time
+                    if (targetPage < frameTable.begin() || targetPage >= frameTable.end())
                     {
-                        // search for the least recently used (referenced) page to evict
+                        // search for the least recently used (referenced) page as the target page
+                        targetPage = frameTable.begin();
+                        targetFrameIndex = targetPage - frameTable.begin();
+                        int lruTime = targetPage->referencedTime;
+
+                        for (vector<Page>::iterator currPage = frameTable.begin() + 1; currPage != frameTable.end(); currPage++)
+                        {
+                            if (lruTime > currPage->referencedTime)
+                            {
+                                lruTime = currPage->referencedTime;
+                                targetPage = currPage;
+                                targetFrameIndex = targetPage - frameTable.begin();
+                            }
+                        }
+
+                        DEBUG(printf(" - Fault, evicting page %d of process %d from frame %d\n", targetPage->refPageIndex, targetPage->processId, targetFrameIndex);)
+
+                        // calculate residence time
+                        procList[targetPage->processId - 1].numOfEvictions++;
+                        procList[targetPage->processId - 1].totalResidencyTime += time - targetPage->allocatedTime;
+
+                        // evict the least recently used (referenced) page
+                        procList[targetPage->processId - 1].pageToFrameIndex[targetPage->refPageIndex] = -1;
+                        targetPage->processId = -1;
 
                     }
+
+                    // allocate page to current frame
+                    targetPage->allocatedTime = time;
+                    targetPage->referencedTime = time;
+                    targetPage->refPageIndex = currRefPageIndex;
+                    targetPage->processId = currProc->id;
+
+                    // log the page to frame index
+                    currProc->pageToFrameIndex[currRefPageIndex] = targetFrameIndex;
+
                 }
+
+                // calculate the next reference index
+                double y = getRandomNumber() / (RAND_MAX + 1.0);
+                nextRefIndex = currProc->refIndex;
+                if (y < currProc->A)
+                    nextRefIndex += 1;
+                else if (y < currProc->A + currProc->B)
+                    nextRefIndex -= 5;
+                else if (y < currProc->A + currProc->B + currProc->C)
+                    nextRefIndex += 4;
+                else
+                    nextRefIndex = getRandomNumber();
+
+                nextRefIndex %= currProc->size;
             }
         }
 
@@ -261,6 +312,7 @@ void LRU()
     }
 
     // output summary
+    printSummary();
 }
 
 int main(int argc, char const *argv[])
@@ -292,7 +344,12 @@ int main(int argc, char const *argv[])
     refCnt = atoi(argv[5]);
     algorithm = argv[6];
 
-    DEBUG(cout << machineSize << " " << pageSize << " " << procSize << " "  << jobMix << " "  << refCnt << " "  << algorithm << endl;)
+    cout << "The machine size is " << machineSize << endl;
+    cout << "The page size is " << pageSize << endl;
+    cout << "The process size is " << procSize << endl;
+    cout << "The job mix number is " << jobMix << endl;
+    cout << "The number of references per process is " << refCnt << endl;
+    cout << "The replacement algorithm is " << algorithm << endl << endl;
 
     // initiate vector of process list and frame table
     initiate(machineSize, pageSize, procSize, jobMix, refCnt);
